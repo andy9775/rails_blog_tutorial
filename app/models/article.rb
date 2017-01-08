b5ea45cba08da19e0dbb50aec95649d5d4630e15
Andy <andy9775@gmail.com>
2017-01-08 14:44:05 -0500

    add comments to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  # ensure comments are deleted along with article
+  has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
